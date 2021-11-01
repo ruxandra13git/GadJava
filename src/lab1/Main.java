65f20fee73235c0d8727505beb7f8b3198000c4b
unknown <ruxandrapreda13@gmail.com>
2021-11-01 21:01:43 +0200

    first lab

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -2,6 +2,8 @@ package lab1;
 
 public class Main {
     public static void main(String[] args) {
+        Exercises ex = new Exercises();
+        ex.arrays();
         System.out.println("Atelierul digital Google!");
     }
 }
